6d18c87b905a06e6012d7cc089c1c6283f366083
rambert <189076903+ram6ert@users.noreply.github.com>
2025-04-29 22:40:01 +0800

    feat: bootloader config
    - for stack and physical memory mappings

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -10,9 +10,20 @@ mod sync;
 extern crate alloc;
 
 use bootloader_api::BootInfo;
+use bootloader_api::BootloaderConfig;
+use bootloader_api::config::Mapping;
 use bootloader_api::entry_point;
+use mm::definitions::KERNEL_STACK_BEGIN;
+use mm::definitions::PHYSICAL_MAP_BEGIN;
 
-entry_point!(kernel_main);
+static CONFIG: BootloaderConfig = {
+    let mut cfg = BootloaderConfig::new_default();
+    cfg.mappings.physical_memory = Some(Mapping::FixedAddress(PHYSICAL_MAP_BEGIN as u64));
+    cfg.mappings.kernel_stack = Mapping::FixedAddress(KERNEL_STACK_BEGIN as u64);
+    cfg
+};
+
+entry_point!(kernel_main, config = &CONFIG);
 
 pub fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
     loop {

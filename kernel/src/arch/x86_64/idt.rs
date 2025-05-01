5e822dbbede9638282c8f183ecc5673affbd605c
rambert <189076903+ram6ert@users.noreply.github.com>
2025-05-01 21:46:17 +0800

    revert(idt.rs)/fix: bring with_error_code again

--- kernel/src/arch/x86_64/idt.rs
+++ kernel/src/arch/x86_64/idt.rs
@@ -242,7 +242,8 @@ macro_rules! make_interruption_handler {
                     "pop r15", "pop r14", "pop r13", "pop r12", "pop r11", "pop r10", "pop r9", "pop r8",
                     "pop rsi", "pop rbp", "pop rdx", "pop rcx", "pop rbx", "pop rax",
                     "pop rdi",
-                    "iretq 8",
+                    "add rsp, 8",
+                    "iretq",
                     sym $inner,
                 );
             }

641a3cd0fa38f6e5793d1200bd8f1e39673e110a
rambert <189076903+ram6ert@users.noreply.github.com>
2025-05-01 19:15:13 +0800

    feat: pic configuration

--- kernel/src/arch/mod.rs
+++ kernel/src/arch/mod.rs
@@ -1,2 +1,3 @@
 pub mod x86_64;
 pub use x86_64::utils::{halt, init};
+pub use x86_64::{disable_irq, enable_irq};
--- kernel/src/arch/x86_64/mod.rs
+++ kernel/src/arch/x86_64/mod.rs
@@ -1,5 +1,6 @@
 mod gdt;
 mod idt;
+mod int;
 mod io;
 pub mod logging;
 pub mod mm;
@@ -8,5 +9,8 @@ pub mod utils;
 
 #[allow(unused_imports)]
 pub use gdt::{KERNEL_CODE_DESCRIPTOR, USER_CODE_DESCRIPTOR, load_gdt};
+
+#[allow(unused_imports)]
+pub use int::{disable_irq, enable_irq};
 
 pub use idt::load_idt;

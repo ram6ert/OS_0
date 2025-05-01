943a5c2d5f8d4a3141e96e25bcba0fa8f80054af
rambert <189076903+ram6ert@users.noreply.github.com>
2025-05-01 19:14:40 +0800

    chore: rewrite comments for serial.rs

--- kernel/src/arch/x86_64/serial.rs
+++ kernel/src/arch/x86_64/serial.rs
@@ -10,28 +10,32 @@ pub struct Serial {
 
 impl Serial {
     unsafe fn init(port: u16) {
-        // 关闭中断
-        out8(port + 1, 0x00);
+        unsafe {
+            // disable interruption
+            out8(port + 1, 0x00);
 
-        // 设置访问波特率除数寄存器
-        out8(port + 3, 0x80);
-        // 设置波特率为 115200 / 3 = 38400
-        out8(port + 0, 0x03);
-        out8(port + 1, 0x00);
+            // baudrate register
+            out8(port + 3, 0x80);
+            // baudrate 115200 / 3 = 38400
+            out8(port + 0, 0x03);
+            out8(port + 1, 0x00);
 
-        // 设置访问数据寄存器，设置 8 位数据长度
-        out8(port + 3, 0x03);
+            // data register
+            out8(port + 3, 0x03);
 
-        // 启用并设置 FIFO
-        out8(port + 2, 0xC7);
+            // FIFO
+            out8(port + 2, 0xC7);
 
-        // 配置调制调解器
-        out8(port + 4, 0x0B);
+            // modem
+            out8(port + 4, 0x0B);
+        }
     }
 
     pub unsafe fn write_byte(&mut self, b: u8) {
-        while in8(self.port + 5) & 0x20 == 0 {}
-        out8(self.port, b);
+        unsafe {
+            while in8(self.port + 5) & 0x20 == 0 {}
+            out8(self.port, b);
+        }
     }
 
     const fn new(port: u16) -> Self {

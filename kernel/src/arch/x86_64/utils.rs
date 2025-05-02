80e4b7c69dbb9a31ff3d14688158eaa3ae7c0c09
rambert <189076903+ram6ert@users.noreply.github.com>
2025-05-02 16:57:21 +0800

    rename(arch/utils): move bind_pt_and_switch_stack() to arch/utils

--- kernel/src/arch/x86_64/utils.rs
+++ kernel/src/arch/x86_64/utils.rs
@@ -73,3 +73,14 @@ pub unsafe fn get_current_page_table_frame() -> Frame {
     Frame::new((cr3 >> 12) & ((1 << 36) - 1))
 }
 
+#[inline(always)]
+pub unsafe fn bind_pt_and_switch_stack(pt: &X86PageTable, stack: u64, callback: fn() -> !) -> ! {
+    unsafe {
+        pt.bind();
+        asm!(
+            "mov rsp, {}",
+            in(reg) stack,
+        );
+    }
+    callback();
+}
